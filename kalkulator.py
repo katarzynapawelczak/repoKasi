c4021f3a86677e7e6cc08d77738b2ff66c6384cb
kasia <katarzynapawelczak@gmail.com>
2014-05-15 17:21:40 +0200

    poprawka dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,7 @@
 def dodaj(a, b):
     return a+b
-    
-dodaj()
+
+liczba1 = int(input("Wprowadz pierwsza liczbe: "))
+liczba2 = int(input("Wprowadz druga liczbe: "))
+print(dodaj(liczba1, liczba2))
 
